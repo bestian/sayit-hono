c868a51ccecaf3b0d7650dd6fb2a02bb326e9ccb
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-12-05 14:29:56 +0800

    完成components處理, close #7

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+	<header class="full-page__row navbar">
+		<div class="full-page__unit">
+			<h1>
+				<a href="/">SayIt</a>
+			</h1>
+			<ul class="inline-list left">
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/speakers">Speakers</a>
+				</li>
+				<li>
+					<a href="/speeches">Speeches</a>
+				</li>
+				<li>
+					<a href="/about">About</a>
+				</li>
+			</ul>
+			<ul class="unstyled-list right"></ul>
+		</div>
+	</header>
+</template>
+
+<style>
+.navbar {
+	padding: 16px 0;
+}
+</style>
+
