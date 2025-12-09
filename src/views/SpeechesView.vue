5ecd725e3d532d8b6ff803a8d9bf219cd25a024f
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-12-09 14:49:06 +0800

    複刻演講清單頁樣稿. close #9

--- src/views/SpeechesView.vue
+++ src/views/SpeechesView.vue
@@ -13,21 +13,24 @@ defineProps<{
 <template>
 	<div class="page">
 		<Navbar />
-		<main class="page__content">
-			<h1>Speeches</h1>
-			<p>以下列表會透過 D1 的 <code>speech_index</code> 資料表生成。</p>
-
-			<ul class="speech-list" v-if="speeches.length > 0">
-				<li v-for="speech in speeches" :key="speech.filename">
-					<a class="speech-link" :href="`/${encodeURIComponent(speech.filename)}`">
-						{{ speech.display_name }}
-					</a>
-				</li>
-			</ul>
-			<p v-else class="empty">暫無資料，請確認 D1 內是否已有 speech_index。</p>
-
-			<p class="source" v-if="source">資料來源：{{ source }}</p>
-		</main>
+		<div class="full-page">
+			<div class="full-page__row">
+				<div class="full-page__unit">
+					<div class="page-header">
+						<h1>Speeches</h1>
+					</div>
+					<ul class="unstyled">
+						<li v-for="speech in speeches" :key="speech.filename">
+							<span class="section-title">
+							<a :href="`/${encodeURIComponent(speech.filename)}`">
+								{{ speech.display_name }}
+							</a>
+							</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 		<Footer />
 	</div>
 </template>
