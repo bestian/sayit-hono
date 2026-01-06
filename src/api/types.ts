dac87321d340044fc88a029c2aa1f300267b3ba5
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-01-07 04:28:38 +0800

    實作快取功能, work on #33

--- src/api/types.ts
+++ src/api/types.ts
@@ -3,6 +3,7 @@ export type ApiEnv = {
 		DB: D1Database;
 		ASSETS: Fetcher;
 		SPEECH_AN: R2Bucket;
+		SPEECH_CACHE: R2Bucket;
 		AUDREYT_TRANSCRIPT_TOKEN?: string;
 		BESTIAN_TRANSCRIPT_TOKEN?: string;
 	};
