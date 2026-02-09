9e2a9f1fa8dfbe44e518ee2373a989f29558b384
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-02-10 04:40:25 +0800

    Update init-speech_index.sql

--- sql/init-speech_index.sql
+++ sql/init-speech_index.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS speech_index (
     -- speakers TEXT
 );
 
-insert into speech_index (filename, display_name, isNested, nest_filenames, nest_display_names)
-values ('2025-11-10-柏林自由會議ai-的角色', '2025-11-10 柏林自由會議：AI 的角色', 0, '', '');
-insert into speech_index (filename, display_name, isNested, nest_filenames, nest_display_names)
-values ('2025-11-10-berlin-freedom-conference-the-role-of-a', '2025-11-10 Berlin Freedom Conference: The Role of AI', 0, '', '');
+-- insert into speech_index (filename, display_name, isNested, nest_filenames, nest_display_names)
+-- values ('2025-11-10-柏林自由會議ai-的角色', '2025-11-10 柏林自由會議：AI 的角色', 0, '', '');
+-- insert into speech_index (filename, display_name, isNested, nest_filenames, nest_display_names)
+-- values ('2025-11-10-berlin-freedom-conference-the-role-of-a', '2025-11-10 Berlin Freedom Conference: The Role of AI', 0, '', '');
 
