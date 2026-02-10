d3656f58cc26cc8a881d81f8149baf4d851b2397
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-02-10 09:14:33 +0800

    footer add bestian as a co-maintainer, close #56

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,8 @@
 		<div class="row">
 			<div class="columns small-12 large-9" style="padding: 0">
 			<p style="font-size: 1em; font-weight: bold; display: flex; margin-inline-end: auto;" id="cc">
-				本站由 唐鳳 維運，除另有標示外，內容以創用 CC0 授權條款釋出<br>
-				This site is maintained and operated by Audrey Tang. Unless otherwise indicated, the content is released
-				under the terms of the Creative Commons CC0 license.
+				本站由 唐鳳 與 唐宗浩 共同維運，除另有標示外，內容以創用 CC0 授權條款釋出<br>
+				This site is co-maintained by Audrey Tang and Bestian Tang. Unless otherwise indicated, the content is released under the terms of the Creative Commons CC0 license.
 			</p>
 			</div>
 			<div class="columns small-12 large-3" style="padding: 0">
